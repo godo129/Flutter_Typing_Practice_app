e8e23d85730afb5256b9e3c203aba3df34a478ea
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-10 15:08:19 +0900

    타이머 추가

--- lib/screens/typing_practice_screen.dart
+++ lib/screens/typing_practice_screen.dart
@@ -1,19 +1,58 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:typing_practice_app/widgets/typing_practicing.dart';
 
-class PracticingScreen extends StatelessWidget {
-
+class PracticingScreen extends StatefulWidget {
   static const routeName = '/practicing-room';
 
   @override
-  Widget build(BuildContext context) {
+  State<PracticingScreen> createState() => _PracticingScreenState();
+}
 
+class _PracticingScreenState extends State<PracticingScreen> {
+  Timer? timer;
+  Duration duration = Duration();
+
+  void setTimer() { 
+    timer = Timer.periodic(Duration(seconds: 1), (_) => addTime());
+  }
+
+  void addTime() {
+    final addSeconds = 1;
+
+    setState(() {
+      final seconds = duration.inSeconds + addSeconds;
+      duration = Duration(seconds: seconds);
+    });
+  }
+
+  @override
+  void initState() {
+    setTimer();
+    // TODO: implement initState
+    super.initState();
+    
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String strDigits(int n) => n.toString().padLeft(2, '0');
+    final seconds = strDigits(duration.inSeconds.remainder(60));
     return Scaffold(
       appBar: AppBar(
         title: Text('타이핑 연습'),
       ),
-      body: TypingPracticeWidget(targetText: 'my name is hong'.split(''),),
+      body: Column(
+          children: <Widget>[
+            Text('${duration.inSeconds}'),
+            TypingPracticeWidget(
+              targetText: 'my name is hong'.split(''),
+            ),
+            TypingPracticeWidget(targetText: 'my name is guliver'.split(''))
+          ]
+        ),
     );
-    
   }
 }
