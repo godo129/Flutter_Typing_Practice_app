d0db7bf07c9fddd0a07b28d61ec07f19020a4943
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-11 15:30:15 +0900

    모델 변경

--- lib/models/texts_model.dart
+++ lib/models/texts_model.dart
@@ -2,11 +2,13 @@ class TextModel {
 
   String title;
   String body; 
+  String language; 
 
   TextModel(
     {
       required this.title,
-      required this.body
+      required this.body,
+      required this.language,
     }
   );
 
