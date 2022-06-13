191535f730ce80741c58e3fe8de783cecd36bde5
godo129 <76652929+godo129@users.noreply.github.com>
2022-06-14 08:39:21 +0900

    debug tag remove

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '타이핑앱',
       theme: ThemeData(
         primarySwatch: Colors.purple,
